262757c292cf3eb773c367949391bf05abf02573
DESKTOP-09H8KCK\bachu <bachuthoihian9x@gmail.com>
2022-06-03 15:54:17 +0700

    part 1 tutorial 1 jetpack

--- app/src/main/java/com/example/materialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/materialjetpackcompose/MainActivity.kt
@@ -2,15 +2,91 @@ package com.example.materialjetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material.Surface
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MaterialTheme {
+                Conversation(SampleData.conversationSample())
+            }
+        }
+    }
+    data class Message(val author: String, val body: String)
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                    )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation() {
+        MaterialTheme {
+            Conversation(SampleData.conversationSample())
+        }
+    }
+    @Composable
+    fun Conversation(messages: List<Message>) {
+        LazyColumn {
+            items(messages) { message ->
+                MessageCard(message)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/materialjetpackcompose/SampleData.kt
@@ -0,0 +1,13 @@
+package com.example.materialjetpackcompose
+
+object SampleData {
+    fun conversationSample(): List<MainActivity.Message> {
+        return listOf(
+            MainActivity.Message("a　三井住友銀行","コミュニケーション"),
+            MainActivity.Message("b 子にwありがとうございました。血","コミュニケーション納品書ハローワーク　リモコン　クレジットカード　エンジニアリング　ルパン三世"),
+            MainActivity.Message("c","c"),
+            MainActivity.Message("d","d"),
+            MainActivity.Message("e","f")
+            )
+    }
+}
